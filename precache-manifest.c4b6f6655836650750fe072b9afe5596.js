e304a9cd41ed00a3bf45d115e0e16137c2ec67c6
hungnguyen0545 <17520545@gm.uit.edu.vn>
2020-01-15 16:46:36 +0700

    Updates

---
+++ precache-manifest.c4b6f6655836650750fe072b9afe5596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcb6f196c5d4c7e751a8e46f889ffbf",
+    "url": "/my-first-React-App/index.html"
+  },
+  {
+    "revision": "7c098831ce5ae95e8397",
+    "url": "/my-first-React-App/static/css/main.82faac22.chunk.css"
+  },
+  {
+    "revision": "a1f9cadb10426753bb90",
+    "url": "/my-first-React-App/static/js/2.429e7af1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-first-React-App/static/js/2.429e7af1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c098831ce5ae95e8397",
+    "url": "/my-first-React-App/static/js/main.1930dd6d.chunk.js"
+  },
+  {
+    "revision": "913f159b1cd98d16a986",
+    "url": "/my-first-React-App/static/js/runtime-main.d3375d5f.js"
+  }
+]);
